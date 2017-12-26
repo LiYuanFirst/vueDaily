d3e7db8606dee72da09ec60542f09fcfeb56be09
LeviLY <15397236229@163.com>
2017-12-26 17:30:46 +0800

    tijiao

---
+++ libs/util.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+const Util = {
+  imgPath: 'http://127.0.0.1:8011/img',
+  apiPath: 'http://127.0.0.1:8010/'
+};
---
+++ proxy.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const request = require('request');
+
+const hostname = '127.0.0.1';
+const port = 8010;
+const imgPort = 8011;
+
+//创建一个API代理服务
+const apiServer = http.createServer((req,res) => {
+   const url = 'http://news-at.zhihu.com/api/4'+req.url;
+   const options = {
+     url : url
+   };
+   function callback(error,response,body){
+     if(!error && response.statusCode === 200){
+       //设置编码类型，解析中文
+       res.setHeader('Content-Type','text/plain;charset=UTF-8');
+       //设置允许跨域
+       res.setHeader('Access-Control-Allow-Orign','*');
+       //返回代理后内容
+       res.end(body);
+     }
+   }
+   request.get(options,callback);
+ });
+ //监听8010端口
+apiServer.listen(port,hostname,() => {
+  console.log('api接口运行中')
+});
+//创建一个图片代理服务
+const imgServer = http.createServer((req,res) => {
+   const url = req.url.split('/img/')[1];
+   const options = {
+     url : url,
+     encoding: null
+   };
+   function callback(error,response,body){
+     if(!error && response.statusCode === 200){
+       //设置编码类型，解析中文
+       res.setHeader('Content-Type',response.headers['Content-Type']);
+       //设置允许跨域
+       res.setHeader('Access-Control-Allow-Orign','*');
+       //返回代理后内容
+       res.end(body);
+     }
+   }
+   request.get(options,callback);
+ });
+ //监听8010端口
+imgServer.listen(imgPort,hostname,()=>{
+  console.log('img接口运行中')
+});
